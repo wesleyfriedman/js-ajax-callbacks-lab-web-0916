c3a7d1c547e2edb5994c2a087b1ac23a1a84fd44
wesleyfriedman <friedman.wesley@gmail.com>
2016-11-10 21:10:08 -0500

    Done

--- index.js
+++ index.js
@@ -6,3 +6,36 @@ function handlebarsSetup() {
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+$('#search').on('click', function(event) {
+  searchRepositories()
+})
+
+function searchRepositories(searchTerms) {
+  var searchTerms = $('input#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+      const template = Handlebars.compile($('#results-template').html())
+
+      $('#results').html(template(data))
+    }).fail(error => {
+      displayError()
+    })
+}
+
+function showCommits(el) {
+
+  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data => {
+      const template = Handlebars.compile($('#commits-template').html())
+      $('#details').html(template(data))
+    }).fail(error => {
+      displayError()
+    })
+}
+
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
+function handlebarsSetup() {
+  Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
+}
